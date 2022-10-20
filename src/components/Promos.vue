5c64728db3d558afe6fcc009679a781a925652bc
sahib <sahib.alejandro@gmail.com>
2022-10-20 00:40:04 -0500

    [wip] Adding promos slider

---
+++ src/components/Promos.vue
@@ -0,0 +1,9 @@
+<script setup>
+import promos from '@/promos';
+</script>
+
+<template>
+  <div class="py-16 text-center rounded bg-slate-200">
+    Promos here!
+  </div>
+</template>
--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import Product from "@/components/Product.vue";
-import products from "@/store/products.js";
+import Product from '@/components/Product.vue';
+import Promos from '@/components/Promos.vue';
+import products from '@/store/products.js';
 </script>
 
 <template>
+  <Promos />
   <h2 class="text-center text-xl font-bold mt-8">Santo Pecado Menú</h2>
   <section class="grid gap-4 mt-4">
     <Product v-for="product in products" v-bind="product" />
